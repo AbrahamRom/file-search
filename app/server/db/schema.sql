f75a7825f716fd29174540aeb30c45872c6d6d77
AbrahamRom <abraham.romero@estudiantes.matcom.uh.cu>
2025-12-10 22:55:43 -0500

    Remove server services and related files for refactoring and restructuring of the application architecture

--- app/server/db/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS files (
-    file_id TEXT PRIMARY KEY,
-    name TEXT NOT NULL,
-    path TEXT NOT NULL,
-    size INTEGER NOT NULL,
-    last_modified DATETIME NOT NULL
-);
